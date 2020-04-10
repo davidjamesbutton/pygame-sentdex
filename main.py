a5527fbe0065b74911116168aba2a02dfcc1bef8
David Button <davidb@pageuppeople.com>
2020-04-10 15:04:44 +1000

    Tutorial 5 - Display text to PyGame screen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import threading
 
 DISPLAY_CAPTION = 'A bit Racey'
 DISPLAY_WIDTH = 800
@@ -21,17 +23,41 @@ car_img = pygame.image.load('racecar.png')
 def car(x, y):
     game_display.blit(car_img, (x, y))
 
+def crash():
+    message_display('You Crashed')
+
+def run_sync(func):
+    thread = threading.Thread(target=func)
+    thread.start()
+    thread.join()
+
+def message_display(text):
+    large_text = pygame.font.Font('freesansbold.ttf', 115)
+    text_surf, text_rect = text_objects(text, large_text)
+    text_rect.center = (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2)
+    game_display.blit(text_surf, text_rect)
+
+    # Run synchronously to ensure display has updated before pausing
+    # https://stackoverflow.com/questions/55881619/sleep-doesnt-work-where-it-is-desired-to/55882173
+    run_sync(lambda: pygame.display.update())
+
+    time.sleep(2)
+    game_loop()
+
+def text_objects(text, font):
+    text_surface = font.render(text, True, COLOUR_BLACK)
+    return text_surface, text_surface.get_rect()
+
 def game_loop():
     x = 0.5 * (DISPLAY_WIDTH - car_img.get_width())
     y = DISPLAY_HEIGHT - 1.5 * car_img.get_height()
 
-    game_running = True
-
-    while game_running:
+    while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_running = False
+                pygame.quit()
+                quit()
             # print(event)
 
         keys = pygame.key.get_pressed()
@@ -46,7 +72,7 @@ def game_loop():
         x += x_change
 
         if x + car_img.get_width() > DISPLAY_WIDTH or x < 0:
-            game_running = False
+            crash()
 
         game_display.fill(COLOUR_WHITE)
         car(x, y)
@@ -56,5 +82,3 @@ def game_loop():
         clock.tick(FPS)
 
 game_loop()
-pygame.quit()
-quit()
