704782f9688f84ed13a3d433bfbfd3b46291ff66
David Button <davidb@pageuppeople.com>
2020-04-10 12:59:14 +1000

    Tutorial 2 - Displaying images with PyGame

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import pygame
 
+DISPLAY_CAPTION = 'A bit Racey'
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+COLOUR_BLACK = (0, 0, 0)
+COLOUR_WHITE = (255, 255, 255)
+
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption(DISPLAY_CAPTION)
+
+game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+car_img = pygame.image.load('racecar.png')
+
+def car(x, y):
+    game_display.blit(car_img, (x, y))
+
+x = 0.5 * (DISPLAY_WIDTH - car_img.get_width())
+y = DISPLAY_HEIGHT - 1.5 * car_img.get_height()
 
 clock = pygame.time.Clock()
-
 crashed = False
 
 while not crashed:
@@ -15,9 +28,13 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
+        #print(event)
+
+    game_display.fill(COLOUR_WHITE)
+    car(x, y)
 
     pygame.display.update()
+
     clock.tick(60)
 
 pygame.quit()
