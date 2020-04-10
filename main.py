767b6d9bbad18bd08203c17399b40fe7ce443f92
David Button <davidb@pageuppeople.com>
2020-04-10 13:21:06 +1000

    Tutorial 4 - Adding boundaries

--- main.py
+++ main.py
@@ -13,43 +13,52 @@ pygame.init()
 pygame.display.set_caption(DISPLAY_CAPTION)
 
 game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+clock = pygame.time.Clock()
+
 car_img = pygame.image.load('racecar.png')
 
 def car(x, y):
     game_display.blit(car_img, (x, y))
 
-x = 0.5 * (DISPLAY_WIDTH - car_img.get_width())
-y = DISPLAY_HEIGHT - 1.5 * car_img.get_height()
-x_change = 0
+def game_loop():
+    x = 0.5 * (DISPLAY_WIDTH - car_img.get_width())
+    y = DISPLAY_HEIGHT - 1.5 * car_img.get_height()
+    x_change = 0
 
-clock = pygame.time.Clock()
-crashed = False
+    game_running = True
 
-while not crashed:
+    while game_running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        # TO DO: Fix car getting stuck when overlapping fast key presses
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -X_SPEED
-            if event.key == pygame.K_RIGHT:
-                x_change = X_SPEED
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_running = False
 
-        # print(event)
+            # TO DO: Fix car getting stuck when overlapping fast key presses
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -X_SPEED
+                if event.key == pygame.K_RIGHT:
+                    x_change = X_SPEED
 
-    x += x_change
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
 
-    game_display.fill(COLOUR_WHITE)
-    car(x, y)
+            # print(event)
 
-    pygame.display.update()
+        x += x_change
 
-    clock.tick(FPS)
+        if x + car_img.get_width() > DISPLAY_WIDTH or x < 0:
+            game_running = False
 
+        game_display.fill(COLOUR_WHITE)
+        car(x, y)
+
+        pygame.display.update()
+
+        clock.tick(FPS)
+
+game_loop()
 pygame.quit()
 quit()
